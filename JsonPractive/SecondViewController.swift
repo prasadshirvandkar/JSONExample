1a2d5d38067c7ea727ecf38fbb98897c6cd0c700
Prasad Shirvandkar <prasadshirvandkar1996@gmail.com>
2016-06-23 23:48:58 +0530

    Delete SecondViewController.swift

--- JsonPractive/SecondViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SecondViewController.swift
-//  JsonPractive
-//
-//  Created by Prasad Shirvandkar on 23/06/16.
-//  Copyright © 2016 Prasad Shirvandkar. All rights reserved.
-//
-
-import Foundation
